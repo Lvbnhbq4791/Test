7dcbe91ef045229ea898558f3d890fb07c0cc5e4
Dmitriy Ogorodnikov <diminskiy_4791@mail.ru>
2023-08-03 08:20:33 +0300

    Отделить от основного комита

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Plus plus = new Plus(5,5);
         Minus minus = new Minus(10,4);
+        System.out.println();
         plus.plus();
         minus.minus();
     }
